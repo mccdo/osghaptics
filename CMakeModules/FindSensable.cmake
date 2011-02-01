bcae97c32f9af1d3941652dcba2b688cd9db879b
burns.don <burns.don@5247ddae-1322-e037-e245-bd00f6b8f98f>
2011-02-01 01:18:42 +0000

    CMakeBuild updates for windows. CMake build is still not ready for prime time.

--- CMakeModules/FindSensable.cmake
+++ CMakeModules/FindSensable.cmake
@@ -1,6 +1,6 @@
 find_path( SENSABLE_INCLUDE HL/hl.h
            HINTS
-           "$ENV{SENSABLE_DIR}"
+           "$ENV{3DTOUCH_BASE}"
            PATH_SUFFIXES include
            PATHS
            /usr
@@ -8,11 +8,13 @@ find_path( SENSABLE_INCLUDE HL/hl.h
 
 find_path( SENSABLE_INCLUDE_UTS HLU/hlu.h
            HINTS
-           "$ENV{SENSABLE_DIR}"
-           PATH_SUFFIXES include/utilities
+           "$ENV{3DTOUCH_BASE}"
+           PATH_SUFFIXES utilities/include
            PATHS
            /usr
            /usr/local )
 
 
 
+#message ( "SENSABLE_INCLUDE is ${SENSABLE_INCLUDE}" )
+#message ( "SENSABLE_INCLUDE_UTS is ${SENSABLE_INCLUDE_UTS}" )
